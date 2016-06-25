9457392c490a25949f3a8b910b7a0370298e3a8a
foxostro <foxostro@gmail.com>
2016-06-24 19:47:09 -0700

    Open blank window configured for OpenGL. Implement basic window event pump.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,66 @@
-#include <cstdio>
-#include "SDL.h"
+//
+//  main.m
+//  PinkTopaz
+//
+//  Created by Andrew Fox on 6/24/16.
+//  Copyright © 2016 Andrew Fox. All rights reserved.
+//
 
-int main(void)
+#include <cstdio>
+#include <OpenGL/gl3.h>
+#include <SDL2/SDL.h>
+#include "config.h"
+
+int main(int argc, char * arg[])
 {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        fprintf(stderr, "SDL_Init failed\n");
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Failed to initialize SDL.\n");
         return EXIT_FAILURE;
     }
-
+    
     SDL_version compiled;
     SDL_version linked;
-
+    
     SDL_VERSION(&compiled);
     SDL_GetVersion(&linked);
-    printf("We compiled against SDL version %d.%d.%d.\n", compiled.major, compiled.minor, compiled.patch);
-    printf("We are linking against SDL version %d.%d.%d.\n", linked.major, linked.minor, linked.patch);
-
+    SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "We compiled against SDL version %d.%d.%d.\n", compiled.major, compiled.minor, compiled.patch);
+    SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "We are linking against SDL version %d.%d.%d.\n", linked.major, linked.minor, linked.patch);
+    
+    SDL_Window *window = SDL_CreateWindow(APP_NAME, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI);
+    
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetSwapInterval(1);
+    SDL_GL_CreateContext(window);
+    
+    int major = 0;
+    int minor = 0;
+    glGetIntegerv(GL_MAJOR_VERSION, &major);
+    glGetIntegerv(GL_MINOR_VERSION, &minor);
+    SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "OpenGL version is %d.%d\n", major, minor);
+    
+    glClearColor(0.2, 0.4, 0.5, 1.0);
+    
+    bool quit = false;
+    
+    while(!quit)
+    {
+        SDL_Event e;
+        
+        while(SDL_PollEvent(&e) != 0)
+        {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+        
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glFlush();
+        SDL_GL_SwapWindow(window);
+    }
+    
+    SDL_DestroyWindow(window);
     SDL_Quit();
+    
     return EXIT_SUCCESS;
 }
