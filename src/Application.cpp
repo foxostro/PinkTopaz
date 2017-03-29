305bb2f43c87a6eac6702b4ffffe548e6a68f6ad
foxostro <foxostro@gmail.com>
2017-03-28 17:49:46 -0700

    Throw an exception if unable to load the terrain texture.

--- src/Application.cpp
+++ src/Application.cpp
@@ -17,6 +17,7 @@
 #include "MouseMoveEvent.hpp"
 #include "Terrain/VoxelDataLoader.hpp"
 #include "Terrain/MesherMarchingCubes.hpp"
+#include "Exception.hpp"
 
 #include "SDL.h"
 #include "SDL_image.h"
@@ -30,6 +31,11 @@ RenderableStaticMesh Application::createTerrainMesh(const std::shared_ptr<Render
     // Load terrain texture array from a single image.
     // TODO: create a TextureArrayLoader class to encapsulate tex loading.
     SDL_Surface *surface = IMG_Load("terrain.png");
+    
+    if (!surface) {
+        throw Exception("Failed to load terrain terrain.png.");
+    }
+
     Renderer::TextureDescriptor texDesc = {
         Renderer::Texture2DArray,
         Renderer::BGRA8,
