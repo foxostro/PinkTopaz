3d14f76f65ca04f72e8c660da77f7fd1ef8802be
foxostro <foxostro@gmail.com>
2017-05-21 00:05:19 -0800

    VoxelData is a GridAddressable object which internally uses an Array3D<Voxel> as an implementation detail. In the future, this will be broken into several chunks.

--- src/Terrain/VoxelData.cpp
+++ src/Terrain/VoxelData.cpp
@@ -9,5 +9,40 @@
 #include "Terrain/VoxelData.hpp"
 
 VoxelData::VoxelData(const AABB &box, const glm::ivec3 &res)
- : Array3D<Voxel>(box, res)
+ : _chunk(box, res)
 {}
+
+Voxel VoxelData::get(const glm::vec3 &p) const
+{
+    return _chunk.get(p);
+}
+
+Voxel& VoxelData::getm(const glm::vec3 &p)
+{
+    return _chunk.getm(p);
+}
+
+Voxel VoxelData::get(const glm::vec3 &p, const Voxel &defaultValue) const
+{
+    return _chunk.get(p, defaultValue);
+}
+
+void VoxelData::set(const glm::vec3 &p, const Voxel &object)
+{
+    return _chunk.set(p, object);
+}
+
+glm::vec3 VoxelData::getCellDimensions() const
+{
+    return _chunk.getCellDimensions();
+}
+
+AABB VoxelData::getBoundingBox() const
+{
+    return _chunk.getBoundingBox();
+}
+
+glm::ivec3 VoxelData::getResolution() const
+{
+    return _chunk.getResolution();
+}
--- src/include/Terrain/VoxelData.hpp
+++ src/include/Terrain/VoxelData.hpp
@@ -13,9 +13,11 @@
 #include "Voxel.hpp"
 
 // A block of voxels in space.
-class VoxelData : public Array3D<Voxel>
+class VoxelData : public GridAddressable<Voxel>
 {
 public:
+    typedef Array3D<Voxel> Chunk;
+    
     // Constructor. Accepts a bounding box decribing the region of space
     // this block of voxels represents. The space is divided into voxel
     // cells at a resolution described by `resolution.' That is, there are
@@ -34,6 +36,31 @@ public:
     
     // Destructor is just the default.
     ~VoxelData() = default;
+    
+    // Each point in space corresponds to exactly one cell. Get the object.
+    Voxel get(const glm::vec3 &p) const override;
+    
+    // Each point in space corresponds to exactly one cell. Get the (mutable) object.
+    Voxel& getm(const glm::vec3 &p) override;
+    
+    // Each point in space corresponds to exactly one cell. Get the object.
+    // If the point is not in bounds then return the specified default value.
+    Voxel get(const glm::vec3 &p, const Voxel &defaultValue) const override;
+    
+    // Each point in space corresponds to exactly one cell. Set the object.
+    void set(const glm::vec3 &p, const Voxel &object) override;
+    
+    // Gets the dimensions of a single cell. (All cells are the same size.)
+    glm::vec3 getCellDimensions() const override;
+    
+    // Gets the region for which the grid is defined.
+    AABB getBoundingBox() const override;
+    
+    // Gets the number of cells along each axis within the valid region.
+    glm::ivec3 getResolution() const override;
+    
+private:
+    Chunk _chunk;
 };
 
 #endif /* VoxelData_hpp */
