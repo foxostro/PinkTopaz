d8684d9b3592fe1dbd049025eefc455961af39ed
foxostro <foxostro@gmail.com>
2017-06-12 22:08:48 -0700

    TerrainMeshes was anemic. Fold it into Terrain.

--- src/Terrain/Terrain.cpp
+++ src/Terrain/Terrain.cpp
@@ -7,19 +7,177 @@
 //
 
 #include "Terrain/Terrain.hpp"
+#include "Terrain/MesherNaiveSurfaceNets.hpp"
+#include "SDL_image.h"
+
+Terrain::~Terrain()
+{
+    std::lock_guard<std::mutex> lock(_lockMeshes);
+    _dispatcher->shutdown();
+}
 
 Terrain::Terrain(const std::shared_ptr<GraphicsDevice> &graphicsDevice,
                  const std::shared_ptr<TaskDispatcher> &dispatcher)
- : _voxels(new VoxelDataStore),
-   _meshes(new TerrainMeshes(graphicsDevice, dispatcher, _voxels))
-{}
+ : _graphicsDevice(graphicsDevice),
+   _dispatcher(dispatcher),
+   _mesher(new MesherNaiveSurfaceNets),
+   _voxels(new VoxelDataStore)
+{
+    // Load terrain texture array from a single image.
+    // TODO: create a TextureArrayLoader class to encapsulate tex loading.
+    SDL_Surface *surface = IMG_Load("terrain.png");
+    
+    if (!surface) {
+        throw Exception("Failed to load terrain terrain.png.");
+    }
+    
+    TextureDescriptor texDesc = {
+        Texture2DArray,
+        BGRA8,
+        static_cast<size_t>(surface->w),
+        static_cast<size_t>(surface->w),
+        static_cast<size_t>(surface->h / surface->w),
+        4,
+        true,
+    };
+    auto texture = graphicsDevice->makeTexture(texDesc, surface->pixels);
+    
+    TextureSamplerDescriptor samplerDesc = {
+        ClampToEdge,
+        ClampToEdge,
+        NearestMipMapNearest,
+        Nearest
+    };
+    auto sampler = graphicsDevice->makeTextureSampler(samplerDesc);
+    
+    StaticMesh mesh; // An empty mesh still has a valid vertex format.
+    auto shader = _graphicsDevice->makeShader(mesh.getVertexFormat(),
+                                              "vert", "frag",
+                                              false);
+    
+    TerrainUniforms uniforms;
+    auto uniformBuffer = _graphicsDevice->makeBuffer(sizeof(uniforms),
+                                                     &uniforms,
+                                                     DynamicDraw,
+                                                     UniformBuffer);
+    uniformBuffer->addDebugMarker("Terrain Uniforms", 0, sizeof(uniforms));
+    
+    // We don't have vertices until the isosurface is extracted later.
+    _defaultMesh = std::make_shared<RenderableStaticMesh>();
+    _defaultMesh->vertexCount = 0;
+    _defaultMesh->buffer = nullptr;
+    _defaultMesh->uniforms = uniformBuffer;
+    _defaultMesh->shader = shader;
+    _defaultMesh->texture = texture;
+    _defaultMesh->textureSampler = sampler;
+    
+    const AABB box = _voxels->boundingBox();
+    const glm::ivec3 res = _voxels->gridResolution() / MESH_CHUNK_SIZE;
+    _drawList = std::make_unique<Array3D<RenderableStaticMesh>>(box, res);
+    _meshes = std::make_unique<Array3D<MaybeTerrainMesh>>(box, res);
+    
+    // When voxels change, we need to extract a polygonal mesh representation
+    // of the isosurface. This mesh is what we actually draw.
+    // For now, we extract the entire isosurface in one step.
+    _voxels->voxelDataChanged.connect([&](const ChangeLog &changeLog){
+        asyncRebuildMeshes(changeLog);
+    });
+}
 
 void Terrain::setTerrainUniforms(const TerrainUniforms &uniforms)
 {
-    _meshes->setTerrainUniforms(uniforms);
+    // The uniforms referenced in the default mesh are also referenced by other
+    // meshes in the terrain. Setting it once here sets it for all of them.
+    _defaultMesh->uniforms->replace(sizeof(uniforms), &uniforms);
 }
 
-void Terrain::draw(const std::shared_ptr<CommandEncoder> &encoder) const
+void Terrain::draw(const std::shared_ptr<CommandEncoder> &encoder)
 {
-    _meshes->draw(encoder);
+    // Update the draw list without blocking long on the meshes lock.
+    // If a mesh is missing then kick off an asynchronous task to generate it.
+    if (_lockMeshes.try_lock()) {
+        _meshes->forEachCell(_meshes->boundingBox(), [&](const AABB &cell){
+            const MaybeTerrainMesh &maybeTerrainMesh = _meshes->get(cell.center);
+            tryUpdateDrawList(maybeTerrainMesh, cell);
+            if (!maybeTerrainMesh) {
+                _dispatcher->async([=]{
+                    rebuildMesh(cell);
+                });
+            }
+        });
+        _lockMeshes.unlock();
+    }
+    
+    // The following resources are referenced by and used by all meshes in the
+    // terrain. We only need to set them once.
+    encoder->setShader(_defaultMesh->shader);
+    encoder->setFragmentSampler(_defaultMesh->textureSampler, 0);
+    encoder->setFragmentTexture(_defaultMesh->texture, 0);
+    encoder->setVertexBuffer(_defaultMesh->uniforms, 1);
+    
+    // Draw all meshes in the draw list.
+    {
+        std::lock_guard<std::mutex> lock(_lockDrawList);
+        for (const auto &drawThis : *_drawList) {
+            if (drawThis.vertexCount > 0) {
+                encoder->setVertexBuffer(drawThis.buffer, 0);
+                encoder->drawPrimitives(Triangles, 0, drawThis.vertexCount, 1);
+            }
+        }
+    }
 }
+
+void Terrain::asyncRebuildMeshes(const ChangeLog &changeLog)
+{
+    // Get the set of meshes which are affected by the changes.
+    std::set<AABB> affectedMeshes;
+    {
+        std::lock_guard<std::mutex> lock(_lockMeshes);
+        for (const auto &change : changeLog) {
+            _meshes->forEachCell(change.affectedRegion, [&](const AABB &cell){
+                affectedMeshes.insert(cell);
+            });
+        }
+    }
+    
+    // Kick off a task to rebuild each affected mesh.
+    for (const AABB &cell : affectedMeshes) {
+        _dispatcher->async([=]{
+            // AFOX_TODO: Keep a sorted list of dirty cells. Instead of
+            // directly specifying it here, have rebuildMesh() pull the next one
+            // off that list in order to decide what gets done next. This could
+            // be the chunk closest to the camera, for example.
+            rebuildMesh(cell);
+        });
+    }
+}
+
+void Terrain::rebuildMesh(const AABB &cell)
+{
+    // Rebuild the mesh associated with the specified cell.
+    _lockMeshes.lock();
+    MaybeTerrainMesh &maybeTerrainMesh = _meshes->mutableReference(cell.center);
+    if (!maybeTerrainMesh) {
+        maybeTerrainMesh.emplace(cell,
+                                 _defaultMesh,
+                                 _graphicsDevice,
+                                 _mesher,
+                                 _voxels);
+    }
+    _lockMeshes.unlock();
+    maybeTerrainMesh->rebuild();
+    tryUpdateDrawList(maybeTerrainMesh, cell);
+}
+
+void Terrain::tryUpdateDrawList(const MaybeTerrainMesh &maybeTerrainMesh,
+                                const AABB &cell)
+{
+    if (maybeTerrainMesh) {
+        auto maybeRenderableMesh = maybeTerrainMesh->nonblockingGetMesh();
+        if (maybeRenderableMesh) {
+            std::lock_guard<std::mutex> lock(_lockDrawList);
+            _drawList->set(cell.center, *maybeRenderableMesh);
+        }
+    }
+}
+
--- src/include/Terrain/Terrain.hpp
+++ src/include/Terrain/Terrain.hpp
@@ -9,37 +9,65 @@
 #ifndef Terrain_hpp
 #define Terrain_hpp
 
-#include "Terrain/VoxelDataStore.hpp"
-#include "Terrain/Mesher.hpp"
-#include "Terrain/VoxelDataGenerator.hpp"
 #include "Renderer/GraphicsDevice.hpp"
-#include "RenderableStaticMesh.hpp"
 #include "TaskDispatcher.hpp"
-#include "TerrainMeshes.hpp"
-#include <mutex>
+#include "Terrain/Mesher.hpp"
+#include "Terrain/VoxelDataStore.hpp"
+#include "Terrain/TerrainMesh.hpp"
+#include "RenderableStaticMesh.hpp"
+#include <experimental/optional>
 
-// This object represents the terrain of the world.
+// Terrain is broken up into several meshes.
 class Terrain
 {
 public:
-    ~Terrain() = default;
+    ~Terrain();
     
-    // Constructor.
     Terrain(const std::shared_ptr<GraphicsDevice> &graphicsDevice,
             const std::shared_ptr<TaskDispatcher> &dispatcher);
     
     // No default constructor.
     Terrain() = delete;
     
-    // Pass a modelview and projection matrix down for use with the terrain.
+    // Passes uniforms down to all terrain meshes.
     void setTerrainUniforms(const TerrainUniforms &uniforms);
     
-    // Draws the terrain.
-    void draw(const std::shared_ptr<CommandEncoder> &encoder) const;
+    // Draws the portions of the terrain which are in view.
+    void draw(const std::shared_ptr<CommandEncoder> &encoder);
     
 private:
+    typedef typename std::experimental::optional<TerrainMesh> MaybeTerrainMesh;
+    
+    static constexpr int MESH_CHUNK_SIZE = 16;
+    
+    void rebuildMeshForChunkInner(const Array3D<Voxel> &voxels,
+                                  const size_t index,
+                                  const AABB &meshBox);
+    
+    void rebuildMeshForChunkOuter(const size_t index,
+                                  const AABB &meshBox);
+    
+    std::shared_ptr<GraphicsDevice> _graphicsDevice;
+    std::shared_ptr<TaskDispatcher> _dispatcher;
+    std::shared_ptr<Mesher> _mesher;
     std::shared_ptr<VoxelDataStore> _voxels;
-    std::shared_ptr<TerrainMeshes> _meshes;
+    std::mutex _lockDrawList;
+    std::unique_ptr<Array3D<RenderableStaticMesh>> _drawList;
+    std::mutex _lockMeshes;
+    std::unique_ptr<Array3D<MaybeTerrainMesh>> _meshes;
+    std::shared_ptr<RenderableStaticMesh> _defaultMesh;
+    
+    // Kicks off asynchronous tasks to rebuild any meshes that are affected by
+    // the specified changes.
+    void asyncRebuildMeshes(const ChangeLog &changeLog);
+    
+    // Rebuilds the one mesh associated with the specified cell.
+    void rebuildMesh(const AABB &cell);
+    
+    // If we can get a hold of the underlying GPU resources then add them to the
+    // draw list.
+    void tryUpdateDrawList(const MaybeTerrainMesh &maybeTerrainMesh,
+                           const AABB &cell);
 };
 
 #endif /* Terrain_hpp */
