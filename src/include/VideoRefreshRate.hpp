96968fb631c7c871299d20d59244228196b64304
foxostro <foxostro@gmail.com>
2017-06-13 18:22:43 -0700

    Added getVideoRefreshRate.

---
+++ src/include/VideoRefreshRate.hpp
@@ -0,0 +1,14 @@
+//
+//  VideoRefreshRate.hpp
+//  PinkTopaz
+//
+//  Created by Andrew Fox on 6/13/17.
+//
+//
+
+#ifndef VideoRefreshRate_hpp
+#define VideoRefreshRate_hpp
+
+double getVideoRefreshRate();
+
+#endif /* VideoRefreshRate_hpp */
---
+++ src/osx/VideoRefreshRate.cpp
@@ -0,0 +1,47 @@
+//
+//  VideoRefreshRate.cpp
+//  PinkTopaz
+//
+//  Created by Andrew Fox on 6/13/17.
+//
+//
+
+#include "VideoRefreshRate.hpp"
+#include "Exception.hpp"
+#include <limits>
+#include <ApplicationServices/ApplicationServices.h>
+
+double getVideoRefreshRate()
+{
+    // Get the lowest refresh rate across all displays. It's safe to refresh at
+    // this rate even if the window moves between displays.
+    
+    constexpr size_t MAX_DISPLAYS = 32;
+    double bestRefreshRate = std::numeric_limits<double>::infinity();
+    
+    CGDirectDisplayID displays[MAX_DISPLAYS];
+    CGDisplayCount count;
+    CGDisplayErr displayErr = CGGetOnlineDisplayList(MAX_DISPLAYS, displays, &count);
+    
+    if(displayErr != 0) {
+        throw Exception("Failed to get the online displays.");
+    }
+    
+    for (CGDisplayCount i = 0; i < count; ++i) {
+        CGDisplayModeRef mode = CGDisplayCopyDisplayMode(displays[i]);
+        if (mode) {
+            double refreshRate = CGDisplayModeGetRefreshRate(mode);
+            CGDisplayModeRelease(mode);
+            
+            if (refreshRate == 0.0) {
+                refreshRate = 60.0;
+            }
+            
+            if (refreshRate < bestRefreshRate) {
+                bestRefreshRate = refreshRate;
+            }
+        }
+    }
+    
+    return bestRefreshRate;
+}
