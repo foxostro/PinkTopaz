e8e3a4ce57cdb084280b58fbb4e38c570e084343
foxostro <foxostro@gmail.com>
2017-06-20 17:53:46 -0700

    VoxelDataStore uses PROFILER again to mark transactions.

--- src/Terrain/VoxelDataStore.cpp
+++ src/Terrain/VoxelDataStore.cpp
@@ -7,4 +7,25 @@
 //
 
 #include "Terrain/VoxelDataStore.hpp"
+#include "Terrain/VoxelData.hpp"
+#include "Profiler.hpp"
 
+VoxelDataStore::VoxelDataStore(const std::shared_ptr<VoxelDataGenerator> &generator,
+                               unsigned chunkSize)
+ : ConcurrentGridMutable<Voxel>(std::make_unique<VoxelData>(generator, chunkSize),
+                                chunkSize)
+{}
+
+void VoxelDataStore::readerTransaction(const AABB &region, const Reader &fn) const
+{
+    // AFOX_TODO: What if we want to copy the region to an Array3D instead
+    // of using a GridView?
+    PROFILER(VoxelDataStoreReader);
+    ConcurrentGridMutable<Voxel>::readerTransaction(region, fn);
+}
+
+void VoxelDataStore::writerTransaction(const AABB &region, const Writer &fn)
+{
+    PROFILER(VoxelDataStoreWriter);
+    ConcurrentGridMutable<Voxel>::writerTransaction(region, fn);
+}
--- src/include/Terrain/VoxelDataStore.hpp
+++ src/include/Terrain/VoxelDataStore.hpp
@@ -9,20 +9,29 @@
 #ifndef VoxelDataStore_hpp
 #define VoxelDataStore_hpp
 
-#include "VoxelData.hpp"
 #include "VoxelDataGenerator.hpp"
 #include "ConcurrentGridMutable.hpp"
 
 // A block of voxels in space with locking and expectation of concurrent access.
-// AFOX_TODO: The VoxelDataStore class has become anemic and should be removed.
 class VoxelDataStore : public ConcurrentGridMutable<Voxel>
 {
 public:
     VoxelDataStore(const std::shared_ptr<VoxelDataGenerator> &generator,
-                   unsigned chunkSize)
-     : ConcurrentGridMutable<Voxel>(std::make_unique<VoxelData>(generator, chunkSize),
-                                    chunkSize)
-    {}
+                   unsigned chunkSize);
+    
+    // Perform an atomic transaction as a "reader" with read-only access to the
+    // underlying data in the specified region.
+    // region -- The region we will be reading from.
+    // fn -- Closure which will be doing the reading.
+    void readerTransaction(const AABB &region, const Reader &fn) const override;
+    
+    // Perform an atomic transaction as a "writer" with read-write access to
+    // the underlying voxel data in the specified region. It is the
+    // responsibility of the caller to provide a closure which will update the
+    // change log accordingly.
+    // region -- The region we will be writing to.
+    // fn -- Closure which will be doing the writing.
+    void writerTransaction(const AABB &region, const Writer &fn) override;
 };
 
 #endif /* VoxelDataStore_hpp */
