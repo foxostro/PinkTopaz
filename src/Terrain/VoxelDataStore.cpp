877d8b4d6b9b7bd51c79e7e6ea829b3658accc58
foxostro <foxostro@gmail.com>
2017-07-25 23:42:25 -0700

    Add a comment about a potential future improvement to VoxelDataStore::readerTransaction.

--- src/Terrain/VoxelDataStore.cpp
+++ src/Terrain/VoxelDataStore.cpp
@@ -18,6 +18,15 @@ VoxelDataStore::VoxelDataStore(const std::shared_ptr<VoxelDataGenerator> &genera
 
 void VoxelDataStore::readerTransaction(const AABB &region, const ArrayReader &fn) const
 {
+    // TODO: The call to copy() will serially fetch the underling voxels if they
+    // were not present. This will lead to other reader transactions being
+    // blocked waiting for those voxels even if they might be able to make
+    // progress if one or two of those voxel chunks became ready.
+    // Instead, we should fire off a group of asynchronous tasks right here
+    // where each task will take the lock on a single voxel chunk and fetch that
+    // chunk. Once all tasks in the group has completed then we grab the entire
+    // lockset and proceed with the copy as before.
+    
     PROFILER(VoxelDataStoreReader);
     LockSet locks(locksForRegion(region));
     auto rawPointer = (VoxelData *)_array.get();
