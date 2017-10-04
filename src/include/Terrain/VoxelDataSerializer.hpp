935a6ed5c706017a6580d5cbcbdbed2f53e91b24
foxostro <foxostro@gmail.com>
2017-10-03 23:04:59 -0700

    Add comments to VoxelDataSerializer.hpp

--- src/include/Terrain/VoxelDataSerializer.hpp
+++ src/include/Terrain/VoxelDataSerializer.hpp
@@ -12,6 +12,7 @@
 #include "Terrain/Voxel.hpp"
 #include "Grid/Array3D.hpp"
 
+// Serializes a chunk of voxels to a sequence of bytes, and back.
 class VoxelDataSerializer
 {
 public:
@@ -26,9 +27,17 @@ public:
     VoxelDataSerializer();
     ~VoxelDataSerializer() = default;
     
+    // Create a chunk of voxels from the provided sequence of bytes.
+    // The bounding box is specified here as that information is not stored in
+    // the serialized representation of the chunk. (This lets serialized chunks
+    // be moved in space.)
     Array3D<Voxel> load(const AABB &boundingBox,
                         const std::vector<uint8_t> &bytes);
     
+    // Serialize the provided voxel chunk to a sequence of bytes.
+    // The serialized representation is not guaranteed to be portable across
+    // different systems. For example, we do nothing to address endianness or
+    // differences in struct member alignment.
     std::vector<uint8_t> store(const Array3D<Voxel> &voxels);
     
 private:
