c0009ef000f83b1d8f46f00c53c77e17d19db23e
foxostro <foxostro@gmail.com>
2017-11-22 16:39:11 -0800

    Add more debug information to OutOfBounds exception in slice() function.

--- src/include/Grid/GridIndexerRange.hpp
+++ src/include/Grid/GridIndexerRange.hpp
@@ -177,7 +177,10 @@ static inline auto slice(const GridIndexer &grid, const AABB &region)
 {
 #ifdef EnableVerboseBoundsChecking
     if (!grid.inbounds(region)) {
-        throw OutOfBoundsException();
+        std::string bboxStr = grid.boundingBox().to_string();
+        std::string regionStr = region.to_string();
+        throw OutOfBoundsException("grid.boundingBox=%s ; region=%s",
+                                   result, bboxStr.c_str(), regionStr.c_str());
     }
 #endif
     
