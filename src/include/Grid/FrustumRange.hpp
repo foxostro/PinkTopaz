b78a79ad828896039c838b3ad44c58168617b31d
foxostro <foxostro@gmail.com>
2018-05-11 21:56:41 -0700

    Use Boost.Coroutine2 to implement a lazy FrustumRange.

--- src/include/Grid/FrustumRange.hpp
+++ src/include/Grid/FrustumRange.hpp
@@ -12,67 +12,51 @@
 #include "GridIndexer.hpp"
 #include "Frustum.hpp"
 
-// Range over grid cells which fall within a specified frustum.
-class FrustumRange
+#include <boost/coroutine2/all.hpp>
+
+namespace FrustumRange {
+
+// Iterate over cells which fall within the specified frustum.
+static inline void forEachCell(boost::coroutines2::coroutine<glm::ivec3>::push_type &sink,
+                               size_t depth,
+                               size_t depthOfLeaves,
+                               const AABB &box,
+                               const Frustum &frustum,
+                               const AABB &sliceBoundingBox,
+                               const GridIndexer &grid)
 {
-public:
-    FrustumRange() = delete;
-    
-    FrustumRange(const Frustum &frustum,
-                 const AABB &sliceBoundingBox,
-                 const GridIndexer &grid)
-    {
-        const auto &res = grid.gridResolution();
-        const auto &bbox = grid.boundingBox();
-        assert((res.x == res.y) && (res.x == res.z));
-        assert(isPowerOfTwo(res.x));
-        forEachCell(0, ilog2(res.x), bbox, frustum, sliceBoundingBox,
-                    _cells, grid);
-    }
-    
-    inline auto begin() const
-    {
-        return _cells.begin();
-    }
-    
-    inline auto end() const
-    {
-        return _cells.end();
-    }
-    
-private:
-    std::vector<glm::ivec3> _cells;
-    
-    // Iterate over cells which fall within the specified frustum.
-    void forEachCell(size_t depth,
-                     size_t depthOfLeaves,
-                     const AABB &box,
-                     const Frustum &frustum,
-                     const AABB &sliceBoundingBox,
-                     std::vector<glm::ivec3> &cells,
-                     const GridIndexer &grid) const
-    {
-        if (frustum.boxIsInside(box) &&
-            frustum.boxIsInside(sliceBoundingBox)) {
-            
-            if (depth == depthOfLeaves) {
-                cells.push_back(grid.cellCoordsAtPoint(box.center));
-            } else {
-                for (auto &octant : box.octants()) {
-                    forEachCell(depth+1, depthOfLeaves, octant, frustum,
-                                sliceBoundingBox, cells, grid);
-                }
+    if (frustum.boxIsInside(box) &&
+        frustum.boxIsInside(sliceBoundingBox)) {
+        
+        if (depth == depthOfLeaves) {
+            glm::ivec3 point = grid.cellCoordsAtPoint(box.center);
+            sink(point);
+        } else {
+            for (auto &octant : box.octants()) {
+                forEachCell(sink, depth+1, depthOfLeaves, octant, frustum,
+                            sliceBoundingBox, grid);
             }
         }
     }
-};
+}
+    
+} // namespace FrustumRange
 
 // Return a range object which can iterate over a frustum within the grid.
-static inline auto slice(const GridIndexer &grid,
-                         const Frustum &frustum,
-                         const AABB &sliceBoundingBox)
+static inline
+boost::coroutines2::coroutine<glm::ivec3>::pull_type
+slice(const GridIndexer &grid,
+      const Frustum &frustum,
+      const AABB &sliceBoundingBox)
 {
-    return FrustumRange(frustum, sliceBoundingBox, grid);
+    const auto res = grid.gridResolution();
+    const auto bbox = grid.boundingBox();
+    assert((res.x == res.y) && (res.x == res.z));
+    assert(isPowerOfTwo(res.x));
+    return boost::coroutines2::coroutine<glm::ivec3>::pull_type([&](boost::coroutines2::coroutine<glm::ivec3>::push_type &sink){
+        FrustumRange::forEachCell(sink, 0, ilog2(res.x), bbox, frustum,
+                                  sliceBoundingBox, grid);
+    });
 }
 
 #endif /* FrustumRange_hpp */
