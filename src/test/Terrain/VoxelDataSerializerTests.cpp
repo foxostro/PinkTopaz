9d594cdeba59923b49848fe3c62902e700a7e200
foxostro <foxostro@gmail.com>
2018-05-18 20:24:00 -0700

    Fix VoxelDataSerializerTests unit tests broken when VoxelDataSource was introduced.

--- src/test/Terrain/VoxelDataSerializerTests.cpp
+++ src/test/Terrain/VoxelDataSerializerTests.cpp
@@ -15,10 +15,11 @@ TEST_CASE("Test Voxel Serializer Round Trip", "[VoxelDataSerializer]") {
     VoxelDataSerializer serializer;
     const AABB region{{16, 16, 16},{16, 16, 16}};
     
-    const auto original = generator.copy(region);
-    const auto bytes = serializer.store(original);
-    const auto reconstructed = serializer.load(region, bytes);
-    REQUIRE(original == reconstructed);
+    generator.readerTransaction(region, [&](const Array3D<Voxel> &original){
+        const auto bytes = serializer.store(original);
+        const auto reconstructed = serializer.load(region, bytes);
+        REQUIRE(original == reconstructed);
+    });
 }
 
 TEST_CASE("Test Voxel Serializer Pad The End", "[VoxelDataSerializer]") {
@@ -26,15 +27,16 @@ TEST_CASE("Test Voxel Serializer Pad The End", "[VoxelDataSerializer]") {
     VoxelDataSerializer serializer;
     const AABB region{{16, 16, 16},{16, 16, 16}};
     
-    const auto original = generator.copy(region);
-    auto bytes = serializer.store(original);
-    
-    // Make sure we can reconstruct the chunk even after padding the end of
-    // the serialized data.
-    for (size_t i = 0; i < 100; ++i) {
-        bytes.push_back(rand() % 255);
-    }
-    
-    const auto reconstructed = serializer.load(region, bytes);
-    REQUIRE(original == reconstructed);
+    generator.readerTransaction(region, [&](const Array3D<Voxel> &original){
+        auto bytes = serializer.store(original);
+        
+        // Make sure we can reconstruct the chunk even after padding the end of
+        // the serialized data.
+        for (size_t i = 0; i < 100; ++i) {
+            bytes.push_back(rand() % 255);
+        }
+        
+        const auto reconstructed = serializer.load(region, bytes);
+        REQUIRE(original == reconstructed);
+    });
 }
