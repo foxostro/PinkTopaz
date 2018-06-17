cd8275a93ea34a56f640f915d4b6c769e82e9dc2
foxostro <foxostro@gmail.com>
2018-06-16 21:14:59 -0700

    Fix VoxelDataSerializerTests.
    These have been broken since VoxelDataChunk was added as we no longer
    serialize/deserialize Array3D<Voxel> directly. Instead, we have the ability
    to compactly represent Sky and Ground chunks without an array.

--- src/test/Terrain/VoxelDataSerializerTests.cpp
+++ src/test/Terrain/VoxelDataSerializerTests.cpp
@@ -9,32 +9,34 @@
 #include "catch.hpp"
 #include "Terrain/VoxelDataSerializer.hpp"
 #include "Terrain/VoxelDataGenerator.hpp"
+#include "Terrain/VoxelDataChunk.hpp"
 
-TEST_CASE("Test Voxel Serializer Round Trip", "[VoxelDataSerializer]") {
+TEST_CASE("Test Voxel Serializer Round Trip for Array Chunks", "[VoxelDataSerializer]") {
     VoxelDataGenerator generator(0);
     VoxelDataSerializer serializer;
     const AABB region{{16, 16, 16},{16, 16, 16}};
-    
-    const auto original = generator.copy(region);
-    const auto bytes = serializer.store(original);
-    const auto reconstructed = serializer.load(region, bytes);
-    REQUIRE(original == reconstructed);
+    const VoxelDataChunk originalChunk = VoxelDataChunk::createArrayChunk(generator.copy(region));
+    const auto serializedBytes = serializer.store(originalChunk);
+    const VoxelDataChunk reconstructedChunk = serializer.load(region, serializedBytes);
+    REQUIRE(originalChunk.getUncompressedBytes() == reconstructedChunk.getUncompressedBytes());
 }
 
-TEST_CASE("Test Voxel Serializer Pad The End", "[VoxelDataSerializer]") {
-    VoxelDataGenerator generator(0);
+TEST_CASE("Test Voxel Serializer Round Trip for Sky Chunks", "[VoxelDataSerializer]") {
     VoxelDataSerializer serializer;
     const AABB region{{16, 16, 16},{16, 16, 16}};
-    
-    const auto original = generator.copy(region);
-    auto bytes = serializer.store(original);
-    
-    // Make sure we can reconstruct the chunk even after padding the end of
-    // the serialized data.
-    for (size_t i = 0; i < 100; ++i) {
-        bytes.push_back(rand() % 255);
-    }
-    
-    const auto reconstructed = serializer.load(region, bytes);
-    REQUIRE(original == reconstructed);
+    const glm::ivec3 gridResolution(32);
+    const VoxelDataChunk originalChunk = VoxelDataChunk::createSkyChunk(region, gridResolution);
+    const auto serializedBytes = serializer.store(originalChunk);
+    const VoxelDataChunk reconstructedChunk = serializer.load(region, serializedBytes);
+    REQUIRE(originalChunk.getUncompressedBytes() == reconstructedChunk.getUncompressedBytes());
+}
+
+TEST_CASE("Test Voxel Serializer Round Trip for Ground Chunks", "[VoxelDataSerializer]") {
+    VoxelDataSerializer serializer;
+    const AABB region{{16, 16, 16},{16, 16, 16}};
+    const glm::ivec3 gridResolution(32);
+    const VoxelDataChunk originalChunk = VoxelDataChunk::createGroundChunk(region, gridResolution);
+    const auto serializedBytes = serializer.store(originalChunk);
+    const VoxelDataChunk reconstructedChunk = serializer.load(region, serializedBytes);
+    REQUIRE(originalChunk.getUncompressedBytes() == reconstructedChunk.getUncompressedBytes());
 }
